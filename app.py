d7d7525cd6099a49c0de07aa507be5f0156fc404
unknown <RuanGrah>
2024-07-12 20:42:33 -0300

    Criando biblioteca

--- app.py
+++ app.py
@@ -2,16 +2,40 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/livros/<int:id>", methods=["Get"])
+livros = []
+
+@app.route("/livros/<int:id>", methods=["GET"])
 def get_livro(id):
     print(" ID Livro " + str(id))
     return jsonify("{'nome': 'livro Python 21 dias}")
 
-@app.route("/livros/<int:id>", methods=["Post"])
+@app.route("/livros", methods=["POST"])
 def create_livro():
-    data = request.get_json
+    data = request.get_json()
+    print(data)
+    livros.append(data)
+    return jsonify(data)
+
+@app.route("/livros/<int:id>", methods = ["PUT"])
+def alterar_livro(id):
+    data = request.get_json()
+    print("ID .: " + str(id))
     print(data)
     return jsonify(data)
+
+@app.route("/livros/<int:id>", methods =["DELETE"])
+def excluir_livro(id): 
+    print("ID" + str(id))
+    return jsonify({"message": "Livro deletado com sucesso"})
+
+@app.route("/livros", methods=["GET"])
+def listar_todos():
+    return jsonify(livros)
+
+
+
+
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
