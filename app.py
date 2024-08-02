9244c6a96f22fb25c2b29575d7d5d0368e553dd1
RuanGrah <ruanfilipegrah@gmail.com>
2024-08-01 21:40:02 -0300

    cadastro autor

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request
+
+from cadastro_livro import (alterar, consultar, consultar_por_id, deletar,
+                            inserir)
+from conexao import conecta_db
+from cadastro_autor import listar_autores
+
+app = Flask(__name__)
+
+@app.route("/livros/<int:id>", methods=["GET"])
+def get_livro(id):
+   conexao = conecta_db()
+   livros = consultar_por_id(conexao,id)
+   return jsonify(livros)
+
+@app.route("/livros", methods=["POST"])
+def inserir_livro():
+    conexao = conecta_db()
+    data = request.get_json()
+    nome=data["nome"]
+    inserir(conexao,nome)
+    return jsonify(data)
+
+@app.route("/livros/<int:id>", methods=["PUT"])
+def alterar_livro(id):
+    conexao = conecta_db()
+    data = request.get_json()
+    nome=data["nome"]
+    alterar(conexao,int(id),nome)
+    return jsonify(data)
+
+
+@app.route("/livros/<int:id>", methods=["DELETE"])
+def excluir_livro(id):
+    conexao = conecta_db()
+    deletar(conexao,id)
+    return jsonify({"message": "Livro deletado com sucesso" })
+
+
+@app.route("/livros", methods=["GET"])
+def listar_todos():
+    conexao = conecta_db()
+    livros = consultar(conexao)
+    return jsonify(livros)
+
+@app.route("/autores", methods=["GET"])
+def listar_todos_autores():
+    conexao = conecta_db()
+    autores = listar_autores(conexao)
+    return jsonify(autores)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
